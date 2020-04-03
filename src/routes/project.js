1aefd0ad79ddfada444db0477e19a06b0f7db0ba
orcuntuna <turboindirblog@gmail.com>
2020-04-03 21:35:55 +0300

    proje detay listeleme yapıldı

--- src/routes/project.js
+++ src/routes/project.js
@@ -5,6 +5,7 @@ require('dotenv/config')
 const router = express.Router()
 const ProjectModel = require('../models/Project')
 const ApiModel = require('../models/Api')
+const CategoryModel = require('../models/Category')
 const authMiddleware = require('../middlewares/auth')
 
 router.get('/list', authMiddleware, (req, res) => {
@@ -38,6 +39,78 @@ router.get('/list', authMiddleware, (req, res) => {
       }
     },
   )
+})
+
+router.get('/detail/:projectId', authMiddleware, (req, res) => {
+  const logged_in_user = req.next.user
+  const project_id = req.params.projectId
+  ProjectModel.findOne({
+    _id: project_id,
+    owner: logged_in_user._id,
+  }, (err, project_data) => {
+    if(err){
+      res.json({
+        success: false,
+        error: err,
+      })
+    }else{
+      if(!project_data){
+        res.json({
+          success: false,
+          error: 'Proje bulunamadı',
+        })
+      }else{
+        CategoryModel.aggregate(
+          [
+            {
+              $match: {
+                owner: mongoose.Types.ObjectId(logged_in_user._id),
+                project_id: mongoose.Types.ObjectId(project_id),
+              },
+            },
+            {
+              $lookup: {
+                from: 'apis',
+                localField: 'apis_id',
+                foreignField: '_id',
+                as: 'apis',
+              },
+            },
+            {
+              $project: {
+                _id: true,
+                order: true,
+                name: true,
+                "apis._id": true,
+                "apis.order": true,
+                "apis.name": true,
+                "apis.method": true,
+                "apis.description": true,
+              }
+            }
+          ],
+          (err, categories_data) => {
+            if (err) {
+              res.json({
+                success: false,
+                message: 'Proje listeli alınamadı.',
+              })
+            } else {
+              let data = JSON.parse(JSON.stringify(project_data))
+              data.categories = categories_data
+              delete data.categories_id
+              delete data.owner
+              delete data.__v
+              res.json({
+                success: true,
+                data: data,
+              })
+            }
+          },
+        )
+      }
+    }
+  })
 })
 
 router.post('/add', authMiddleware, (req, res) => {
